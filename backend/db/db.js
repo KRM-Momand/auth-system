6d79bf667ec6a4b1d9c5b9df81748a444a336afc
KRM-Momand <kh.momand19@gmail.com>
2026-07-11 00:04:48 +0200

    reg page modifying

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,23 +2,23 @@ const {Pool} = require('pg');
 require('dotenv').config(); 
 
 
-/*
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-}); */
+}); 
 
 
 
-
+/*
 const pool = new Pool({
     user: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT
-}) 
+})  */
 
 module.exports = {pool}; 
